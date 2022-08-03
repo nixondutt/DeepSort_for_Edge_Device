cba0af884f7a225476edec2ef0752128fd7dc1dc
nixon <nixondutta402@gmail.com>
2022-08-03 15:29:21 +0900

    tflite conversion functionality updated

--- deep_sort_pytorch/deep_sort/deep/conversion_edgemodel.py
+++ deep_sort_pytorch/deep_sort/deep/conversion_edgemodel.py
@@ -5,15 +5,17 @@ from torch import nn
 import torch.utils.model_zoo as model_zoo
 import torch.onnx
 import torch
+import tensorflow as tf
 import torchvision.transforms as transforms
 import numpy as np
 import cv2
 import logging
 import onnx
 import onnxruntime
-
+from onnx_tf.backend import prepare
 # from .model import Net
 from model import Net
+
 
 
 class Extractor(object):
@@ -66,10 +68,30 @@ class Extractor(object):
         np.testing.assert_allclose(Extractor.to_numpy(torch_out), ort_outs[0], rtol=1e-03, atol=1e-05)
         
         print("Tested with Onnxruntime, result OK!!")
+    
+    def tflite_conversion(self,onnx_model_path):
+        # def representative_dataset():
+        #     for _ in range(100):
+        #         data = np.random.rand(1,3, 128, 64)
+        #         yield [data.astype(np.float32)]
+        onnx_model = onnx.load(onnx_model_path)
+        inputs = onnx_model.graph.input
+        for input in inputs:
+            print(input.type.tensor_type.shape)
+        tf_rep = prepare(onnx_model)
+        tf_rep.export_graph("checkpoint/saved_model.pb")
+        converter = tf.lite.TFLiteConverter.from_saved_model("checkpoint/saved_model.pb")
+        # converter.optimizations = [tf.lite.Optimize.DEFAULT]
+        # converter.representative_dataset = representative_dataset
+        # converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+        # converter.inference_input_type = tf.int8  # or tf.uint8
+        # converter.inference_output_type = tf.int8  # or tf.uint8
+        tflite_model = converter.convert()
+        # Save the model.
+        with open('checkpoint/deepsort.tflite', 'wb') as f:
+            f.write(tflite_model)
 
 
-
-        
 
 if __name__ == '__main__':
     img = cv2.imread("demo.jpg")[:, :, (2, 1, 0)]
@@ -78,3 +100,4 @@ if __name__ == '__main__':
     extr.net.eval()
     onnx_model_path = "checkpoint/deepsort.onnx"
     extr.onnx_conversion(extr,onnx_model_path)
+    extr.tflite_conversion(onnx_model_path)
